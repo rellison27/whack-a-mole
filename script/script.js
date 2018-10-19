0ed160243949b1849612d651a1a4f204ddf7ac19
rellison27 <rellison27@gmail.com>
2018-10-19 10:25:18 -0400

    WIP- this actually has the fix. for randomPit() and game runs for 10 seconds

--- script/script.js
+++ script/script.js
@@ -1,32 +1,48 @@
 //grab all classes from html
-var mole = document.querySelectorAll(".square")
-var points = document.querySelectorAll(".points")
-var pits = document.querySelectorAll(".dirt") 
-let pointInc = 0;
+const mole = document.querySelectorAll('.square')
+const newPoints = document.querySelectorAll('.points')
+const pits = document.querySelectorAll('.dirt') 
+let points = 0;
 let timerDone = false;
 
-//function that randomly selects a dirt class for a mole
-function randomMole (pits) {
-    const Onemole = Math.round(Math.random() * pits.length);
-    return pits[Onemole]
+//function that randomly selects a dirt class for a mole to emerge from
+function randomPit(pits) {
+    //Math.flooor because round was returning higher indecies
+    const Onepit = Math.floor(Math.random() * pits.length);
+    const pit = pits[Onepit]
+    return pit
 }
 //mole needs to emerge for a random amount of time
 function moleWindow() {
-    var min = 1,
-      max = 10;
-    var rand = Math.floor(Math.random() * (max - min + 1) + min); //Generate Random number between 1 - 10
-  }
+    const min = 700,
+         max = 1000;
+     return Math.round(Math.random() * (max - min + 1) + min); //Generate Random number between 700 - 1000
+}
 function emerge () {
     const emergeWindow = moleWindow();
-    const moley = randomMole(pits)
-    moley.classList.add("push");
+    const pit = randomPit(pits);
+    pit.classList.add("push");
+    setTimeout(() => {     
+        !timerDone ? emerge() : null; // make sure you put stop() instead of null
+        pit.classList.remove("push");
+	}, emergeWindow)
 }
+//digglit needs to re-submerge
+
 // The amount of time each mole stays visible should be random.
 
 //function that starts the game
+function begin() {
+    emerge();
+    timerDone = false;
+    setTimeout(() => {
+		timerDone = true;
+	}, 10000);
+
+}
 
 //function that stops game
 
-//function that pauses the game
+//function that resets the game
 
 //function for clicks on mole to increase score
