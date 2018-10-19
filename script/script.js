79d94d473477fd1e3116f3c333ea711264114d3a
rellison27 <rellison27@gmail.com>
2018-10-19 18:21:18 -0400

    Whack-A-Diglett: fully functional game

--- script/script.js
+++ script/script.js
@@ -1,48 +1,70 @@
 //grab all classes from html
-const mole = document.querySelectorAll('.square')
-const newPoints = document.querySelectorAll('.points')
-const pits = document.querySelectorAll('.dirt') 
+const diglett = document.querySelectorAll(".diglett");
+const newPoints = document.querySelector(".points");
+const pits = document.querySelectorAll(".dirt");
 let points = 0;
 let timerDone = false;
+let pausedGame = false;
 
-//function that randomly selects a dirt class for a mole to emerge from
-function randomPit(pits) {
-    //Math.flooor because round was returning higher indecies
-    const Onepit = Math.floor(Math.random() * pits.length);
-    const pit = pits[Onepit]
-    return pit
+//function that randomly selects a dirt class for a diglett to emerge from
+function pickPit(pits) {
+  //Math.flooor because round was returning higher indices
+  const Onepit = Math.floor(Math.random() * pits.length);
+  const pit = pits[Onepit];
+  return pit;
 }
-//mole needs to emerge for a random amount of time
-function moleWindow() {
-    const min = 700,
-         max = 1000;
-     return Math.round(Math.random() * (max - min + 1) + min); //Generate Random number between 700 - 1000
-}
-function emerge () {
-    const emergeWindow = moleWindow();
-    const pit = randomPit(pits);
-    pit.classList.add("push");
-    setTimeout(() => {     
-        !timerDone ? emerge() : null; // make sure you put stop() instead of null
-        pit.classList.remove("push");
-	}, emergeWindow)
-}
-//digglit needs to re-submerge
 
-// The amount of time each mole stays visible should be random.
+//diglett needs to emerge for a random amount of time
+function pickTime() {
+  const min = 300,
+    max = 1000;
+  return Math.round(Math.random() * (max - min + 1) + min);
+}
 
-//function that starts the game
+// use the randomly generated time and pit to emerge a diglett
+function emerge() {
+  const emergeWindow = pickTime();
+  const pit = pickPit(pits);
+  pit.classList.add("push");
+  setTimeout(() => {
+    //if true keep going if not STOP
+    if (!timerDone && !pausedGame) emerge();
+    //after time return diglett to its dirt pit
+    pit.classList.remove("push");
+  }, emergeWindow);
+}
+
+//add listener for diglett
+diglett.forEach(diglett => diglett.addEventListener("click", getPoints));
+
+//add points for clicking diglett
+function getPoints(click) {
+  if (click.isTrusted) points += 10;
+  newPoints.textContent = points;
+}
+//function that starts the game - can start after stop, not after a full round
 function begin() {
-    emerge();
-    timerDone = false;
-    setTimeout(() => {
-		timerDone = true;
-	}, 10000);
-
+  points = 0;
+  pausedGame = false;
+  emerge();
+  startTimer();
 }
 
 //function that stops game
+function stop() {
+  pausedGame = true;
+}
 
-//function that resets the game
+// function to begin timer
+function startTimer() {
+  setTimeout(() => {
+    //when game is stopped timer from previous game shouldn't infere
+    if (!pausedGame) timerDone = true;
+  }, 10000);
+}
 
-//function for clicks on mole to increase score
+//function that resets the game and points after you play a full round
+function reset() {
+  newPoints.textContent = 0;
+  timerDone = false;
+}
